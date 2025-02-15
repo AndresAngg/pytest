bda3deb85cdb5f71991fe6210b0a7c9c32519320
Andrés <andresangg@gmail.com>
2025-02-15 10:40:04 -0500

    add tests

--- pages/sanbox_page.py
+++ pages/sanbox_page.py
@@ -3,9 +3,19 @@ from .base_page import BasePage
 
 class SanboxPage(BasePage):
     BTN_HIDDEN = (By.XPATH, "//button[normalize-space()='Hacé click para generar un ID dinámico y mostrar el elemento oculto']")
+    TXT_HIDDEN = (By.XPATH, "//p[@id='hidden-element']")
+    CHECK_BOX = (By.XPATH, "//input[@id='checkbox-1']")
+    
     def __init__(self, driver):
         super().__init__(driver)
         
     def click_btn_hidden(self):
         click_btn_hidden = self.wait_element_present(self.BTN_HIDDEN)
         click_btn_hidden.click()
+    
+    def get_txt_hidden(self):
+        return self.wait_element_present(self.TXT_HIDDEN)
+    
+    def select_check_box(self):
+        checkbox = self.wait_element_present(self.CHECK_BOX)
+        checkbox.click()
--- tests/test_sanbox.py
+++ tests/test_sanbox.py
@@ -4,3 +4,5 @@ from pages.sanbox_page import SanboxPage
 def test_click_btn_hidden(sanbox_page):
     sanbox_page.click_btn_hidden()
     assert sanbox_page.get_txt_hidden().is_displayed()
+    
+    sanbox_page.select_check_box()
